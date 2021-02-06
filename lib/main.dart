b4142dbf8e8c08c3157aabdbba26ee528114a17b
yusufkontoglu <yusufkontoglu@gmail.com>
2021-02-06 17:35:35 +0300

    homeScreen listeleri oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      routes: {
+        '/navigation' : (context) => NavigationPage(),
+        '/home' : (context) => HomePage(),
+      },
+      home: NavigationPage(),
     );
   }
+}
+
+class NavigationPage extends StatefulWidget {
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  ///NavigationBar için sayfalar
+  List<Widget> pages = [
+    HomePage(),
+    HomePage(),//Todo: MyLessonsPage()
+    HomePage(),//Todo: FovuriteInstructersPage()
+  ];
+
+  int pageNum = 0;
+
+  PageController pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Ders Uygulaması'),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(38, 50, 56, 1),
+      ),
+      body: PageView(
+        //BottomNavigationBar bağlantısı
+        controller: pageController,
+        onPageChanged: (_pageNum) {
+          setState(() {
+            pageNum = _pageNum;
+          });
+        },
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(38, 50, 56, 1),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Decoration().homeIcon,
+            ),
+            label: 'Ana sayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Decoration().bookIcon),
+            label: 'Derslerim',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Decoration().favoritesIcon),
+            label: 'Favori Eğitmenler',
+          ),
+        ],
+        currentIndex: pageNum,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            pageNum = index;
+            //PageView bağlantısı
+            pageController.animateToPage(pageNum,
+                duration: Duration(milliseconds: 500), curve: Curves.ease);
+          });
+        },
+      ),
+    );
+  }
+}
+
+class Decoration {
+  IconData notificationIcon = Icons.notifications;
+  IconData searchIcon = Icons.search;
+  IconData userIcon = Icons.account_circle;
+  IconData starIcon = Icons.star;
+  IconData homeIcon = Icons.home;
+  IconData favoritesIcon = Icons.favorite_border_outlined;
+  IconData bookIcon = Icons.book;
 }
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class AvailableLessons extends StatefulWidget {
+  @override
+  _AvailableLessonsState createState() => _AvailableLessonsState();
+}
+
+class _AvailableLessonsState extends State<AvailableLessons> {
+  List lessonsList = [
+    ['lesson1', 'Math', 20],
+    ['lesson2', 'History', 10],
+    ['lesson3', 'Scince', 30],
+    ['lesson4', 'English', 25],
+    ['lesson5', 'Turkish', 22],
+    ['lesson6', 'Deutch', 20],
+    ['lesson7', 'Coding', 18],
+    ['lesson8', 'Scince', 35],
+    ['lesson9', 'Math', 15],
+    ['lesson10', 'Math', 30],
+    ['lesson11', 'Math', 5],
+    ['lesson12', 'Math', 8],
+    ['lesson13', 'Math', 10],
+    ['lesson14', 'Math', 20],
+    ['lesson15', 'Math', 25],
+    ['lesson16', 'Math', 23],
+    ['lesson17', 'Math', 28],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: lessonsList.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Text(lessonsList[index][0]),
+            subtitle: Text(lessonsList[index][1]),
+            trailing: Text('kişi sayısı: ${lessonsList[index][2].toString()}'),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        );
+      },
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,123 +52,125 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //PageView Sayfaları
-  List<Widget> pages = [
-    Container(
-      child: Text('ana sayfa'),
-    ),
-    AvailableLessons(),
-    Container(
-      child: Text('Favori eğitmenler'),
-    ),
-  ];
-  //Başlangıç sayfası
-  int pageNum = 0;
-  PageController pageController = PageController(
-    initialPage: 0,
-  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-
-        title: Text('Ders Uygulaması'),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(38,50,56,1),
-      ),
-      body: PageView(
-        controller: pageController,
-        //BottomNavigationBar bağlantısı
-        onPageChanged: (_pageNum){
-          setState(() {
-            pageNum = _pageNum;
-          });
-        },
-        children: pages,
-      ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Color.fromRGBO(38,50,56,1),
-        items: [
-          BottomNavigationBarItem(
-          icon: Icon(Decoration().homeIcon,),
-          label: 'Ana sayfa',
-        ),
-          BottomNavigationBarItem(
-            icon: Icon(Decoration().bookIcon),
-            label: 'Derslerim',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Öneriler'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Decoration().favoritesIcon),
-            label: 'Favori Eğitmenler',
+          SizedBox(height: 160.0, child: SuggestedBar()),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('filtre bölümü buraya gelecek'),
           ),
+          Expanded(child: TrendsList()),
         ],
-        currentIndex: pageNum,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.white,
-        onTap:(index) {
-          setState(() {
-            pageNum = index;
-            //PageView bağlantısı
-            pageController.animateToPage(pageNum, duration: Duration(milliseconds: 500), curve: Curves.ease);
-          });
-        },
       ),
     );
   }
 }
-class AvailableLessons extends StatefulWidget{
-  @override
-  _AvailableLessonsState createState() => _AvailableLessonsState();
 
-}
-class _AvailableLessonsState extends State<AvailableLessons> {
-  List lessonsList = [
-    ['lesson1', 'Math', 20,],
-    ['lesson2', 'History', 10,],
-    ['lesson3', 'Scince', 30,],
-    ['lesson4', 'English', 25,],
-    ['lesson5', 'Turkish', 22,],
-    ['lesson6', 'Deutch', 20,],
-    ['lesson7', 'Coding', 18,],
-    ['lesson8', 'Scince', 35,],
-    ['lesson9', 'Math', 15,],
-    ['lesson10', 'Math', 30,],
-    ['lesson11', 'Math', 5,],
-    ['lesson12', 'Math', 8,],
-    ['lesson13', 'Math', 10,],
-    ['lesson14', 'Math', 20,],
-    ['lesson15', 'Math', 25,],
-    ['lesson16', 'Math', 23,],
-    ['lesson17', 'Math', 28,],
+class SuggestedBar extends StatelessWidget {
+  final List<String> fakeList = [
+    'Yavuz Selim Yayla ile Temel Yazılım',
+    'Yavuz Selim Yayla ile Temel Yazılım',
+    'Yavuz Selim Yayla ile Temel Yazılım'
   ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: lessonsList.length,
+      itemCount: fakeList.length,
+      scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
         return Card(
-
-          child:  ListTile(
-            title: Text(lessonsList[index][0]),
-            subtitle: Text(lessonsList[index][1]),
-            trailing: Text('kişi sayısı: ${lessonsList[index][2].toString()}'),
-          ),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15)
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.red,
+                  width: 80.0,
+                  height: 80.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(fakeList[index]),
+              ),
+            ],
           ),
         );
-      },);
+      },
+    );
+  }
+}
+
+class TrendsList extends StatefulWidget {
+  @override
+  _TrendsListState createState() => _TrendsListState();
+}
+
+class _TrendsListState extends State<TrendsList> {
+  @override
+  Widget build(BuildContext context) {
+    return buildListView(data);
+    //   FutureBuilder(
+    //   future: null,
+    //   builder: (context, snapshot){
+    //     return buildListView(snapshot.data);
+    //   },
+    // );
   }
 
-}
-class Decoration {
-  IconData notificationIcon = Icons.notifications;
-  IconData searchIcon = Icons.search;
-  IconData userIcon = Icons.account_circle;
-  IconData starIcon = Icons.star;
-  IconData homeIcon = Icons.home;
-  IconData favoritesIcon = Icons.favorite_border_outlined;
-  IconData bookIcon = Icons.book;
+  var data = [
+    'A deneme',
+    'B deneme',
+    'C deneme',
+    'D deneme',
+    'E deneme',
+    'F deneme',
+    'G deneme',
+  ];
 
+  Widget buildListView(List data) {
+    return ListView.builder(
+      itemCount: data.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Text(data[index]),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget LessonCard(){
+  return Container(
+    color: Colors.grey,
+    width: 120.0,
+    height: 120.0,
+  );
+}
+
+Widget SuggestedLessonCard(){
+  return Container(
+    child: Column(
+    children: [
+      LessonCard(),
+      ///Todo: Açıklamaları yaz.
+    ],
+  ),
+  );
 }
