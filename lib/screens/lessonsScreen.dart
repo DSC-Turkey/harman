f3fda12b9afc98da1c6d302079fa9bd6c12e173a
YusufMuharremDaskaya <ydaskaya04@gmail.com>
2021-02-06 18:14:17 +0300

    screens

---
+++ lib/screens/lessonsScreen.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+
+import 'homeScreen.dart';
+
+class MyLessonsPage extends StatefulWidget{
+  @override
+  _MyLessonsPageState createState() => _MyLessonsPageState();
+}
+
+class _MyLessonsPageState extends State<MyLessonsPage> {
+  List lessons = [
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions'],
+    ['title', 'teacher', 'maxStudentValue', 'descriptions']
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: lessons.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                LessonCard(180),
+                SizedBox(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child:  Column(
+                      children: [
+                        Text(lessons[index][0],style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+                        Padding(padding: EdgeInsets.only(bottom: 20), child: Text(lessons[index][1],style: TextStyle(color: Colors.grey),),),
+                        Text(lessons[index][2],style: TextStyle(color: Colors.white),),
+                        Text(lessons[index][3],style: TextStyle(color: Colors.white),)
+                      ],
+                    ),
+                  ),
+                  width: 180.0,
+                )
+
+              ],
+            ),);
+        });
+  }
+
+}
